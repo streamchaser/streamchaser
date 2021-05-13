1d4b3e700267c058f4180026571abd81ec7a4c47
Mikael <mikaelpasovski@gmail.com>
2021-05-13 09:37:48 +0000

    Added expansion of media-card on hover

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -2,9 +2,10 @@
     import {
         Footer,
         Button,
+        Icon
     } from 'svelte-materialify';
-    import { fly } from 'svelte/transition';
-
+    import {fly, slide} from 'svelte/transition';
+    import {mdiChevronUp} from '@mdi/js'
 
     const links = ['FAQ', 'Privacy policy', 'Contact us', 'About us'];
     const logos = {
@@ -15,31 +16,38 @@
 
     let toggle = false;
 
+    function setToggle() {
+        toggle = !toggle;
+    }
+
     export let y = 0;
 
 </script>
 
 <!-- listening for scroll events -->
-<svelte:window bind:scrollY={y} />
+<svelte:window bind:scrollY={y}/>
 
 <Footer padless class="white theme--dark flex-box">
-    <label style="bottom: 50px; position: fixed" class="s-switch">
-    <input type="checkbox" bind:checked={toggle}>
-    ^
-    </label>
+    <div class="icon">
+        <Button text rounded class="black-text" on:click={setToggle}>
+            <Icon class="black-text" path={mdiChevronUp} rotate={toggle ? 180 : 0}/>
+        </Button>
+    </div>
     {#if toggle}
-        <div transition:fly="{{ y: 100, duration: 800 }}" class="footer flex-box" style="padding: 10px">
+        <div transition:slide="{{ y: 100, duration: 800 }}" class="footer flex-box"
+             style="padding: 10px">
             <div class="mt-2 mb-2">
                 {#if links}
                     {#each links as link}
-                      <Button onclick="window.location.href='https://www.youtube.com/watch?v=yPYZpwSpKmA';"
-                              text rounded class="black-text" style="padding: 0 60px 0 60px">{link}</Button>
+                        <Button onclick="window.location.href='https://www.youtube.com/watch?v=yPYZpwSpKmA';"
+                                text rounded class="black-text"
+                                style="padding: 0 60px 0 60px">{link}</Button>
                     {/each}
                 {/if}
-                </div>
-                <div style="float: right">
+            </div>
+            <div style="float: right">
                 {#if logos}
-                    {#each  Object.entries(logos) as [alt, src]}
+                    {#each Object.entries(logos) as [alt, src]}
                         <Button text rounded><img {src} {alt}></Button>
                     {/each}
                 {/if}
@@ -56,20 +64,20 @@
         border-top: solid black;
         border-width: thin;
         width: 100%;
+        z-index: 99;
     }
-
     img {
         max-width: 50px;
         height: auto;
         border-radius: 50%;
     }
-
     div {
         display: inline;
         transition: transform 300ms linear;
     }
-
-    .hide {
-        transform: translateY(100%);
+    .icon {
+        position: fixed;
+        color: black;
+        bottom: 60px;
     }
 </style>
