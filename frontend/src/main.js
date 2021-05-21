bb43dd84e1395a213676649255fea359d4e1f7d6
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2021-05-21 13:52:41 +0200

    Remove components with duplicate code

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,14 +4,3 @@ import App from './App.svelte';
 const app = HMR(App, { target: document.body }, 'routify-app')
 
 export default app;
-
-/*
-// For testing the different components
-import MovieDetailView from './components/MovieDetailView.svelte'
-import TvDetailView from './components/TvDetailView.svelte'
-
-const app = new TvDetailView({
-	target: document.body,
-});
-
-export default app;*/
