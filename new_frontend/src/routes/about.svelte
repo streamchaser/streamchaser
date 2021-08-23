9f7322e788282469813e93ad20a5ac8c711da23c
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2021-08-23 13:48:39 +0200

    Setup svelte-kit with DaisyUI

---
+++ new_frontend/src/routes/about.svelte
@@ -0,0 +1,8 @@
+<script>
+    // import "../styles/tailwind.css";
+</script>
+<div class="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
+    Hello World
+</div>
+<button class="btn btn-primary">DaisyUI Button</button>
+<h1>??????</h1>
---
+++ new_frontend/src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+        import Navbar from '../components/navbar.svelte';
+        import Footer from '../components/footer.svelte';
+	const listOfThings = ['bent', 'ib', 'børge', 'ulla', 'åse'];
+</script>
+
+<Navbar />
+
+<div class="container mx-auto">
+	<div class="grid grid-cols-3 gap-2 p-2 pt-4 bg-base-100">
+		{#each listOfThings as thing}
+			<div class="card bordered w-auto">
+				<figure>
+					<img src="./melting_ice.jpeg" alt="haha du kan ikke se" />
+				</figure>
+				<div class="card-body">
+					<h2 class="card-title">{thing}</h2>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<Footer />
