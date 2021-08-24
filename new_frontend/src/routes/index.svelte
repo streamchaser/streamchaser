f241e7b3118102719b3fe8fbfaa4dd1dabbf52ce
Frederik Primdahl <frederik.vincent@hotmail.com>
2021-08-24 20:48:10 +0200

    Begin work on index page

--- new_frontend/src/routes/index.svelte
+++ new_frontend/src/routes/index.svelte
@@ -1,24 +1,86 @@
 <script>
-	import Navbar from '../components/navbar.svelte';
-	import Footer from '../components/footer.svelte';
-	const listOfThings = ['bent', 'ib', 'børge', 'ulla', 'åse'];
+    import Navbar from '../components/navbar.svelte';
+    import Footer from '../components/footer.svelte';
+
+    const search_url = 'http://localhost:1337/search/';
+    const genre_url = 'http://localhost:1337/genres/';
+    const PROVIDER_URL = 'http://localhost:1337/providers/';
+    const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+    const INPUT_TIMER = 200;
+
+    let currentCountry = 'US'
+    let input = '';
+    let timer;
+    let media = [];
+    let selectedGenres;
+    let selectedProviders;
+    let mappedSelectedGenres;
+    let mappedSelectedProviders;
+
+    // run search if we haven't received input in the last 200ms
+    const debounceInput = () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            input.trim() ? search() : media = [];
+        }, INPUT_TIMER);
+    }
+
+    $: mappedSelectedGenres = selectedGenres ? selectedGenres.map(item => item.value) : [];
+    $: mappedSelectedProviders = selectedProviders ? selectedProviders.map(item => item.value) : [];
+
+
+    const search = async () => {
+        // Builds the optional query for genres
+        // Example: "?g=Action&g=Comedy&g=Drama"
+        if (input) {
+            let genre_query = '';
+            for (let i = 0; i < mappedSelectedGenres.length; i++) {
+                genre_query += `&g=${mappedSelectedGenres[i]}`;
+            }
+            for (let i = 0; i < mappedSelectedProviders.length; i++) {
+                genre_query += `&p=${mappedSelectedProviders[i]}`;
+            }
+
+            const res = await fetch(search_url + input + '?c=' + currentCountry + genre_query);
+            media = await res.json();
+        }
+    };
+
 </script>
 
-<Navbar />
+<Navbar/>
+
 
 <div class="container mx-auto">
-	<div class="grid grid-cols-3 gap-2 p-2 pt-4 bg-base-100">
-		{#each listOfThings as thing}
-			<div class="card bordered w-auto">
-				<figure>
-					<img src="./melting_ice.jpeg" alt="haha du kan ikke se" />
-				</figure>
-				<div class="card-body">
-					<h2 class="card-title">{thing}</h2>
-				</div>
-			</div>
-		{/each}
-	</div>
+    <div class="form-control pt-4">
+        <input
+                type="text"
+                placeholder="Search"
+                class="input input-bordered"
+                bind:value={input}
+                on:input={debounceInput}>
+    </div>
+    {#if media.hits}
+        <div class="grid grid-cols-2 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-2 p-2 pt-4 bg-base-100">
+            {#each media.hits as media}
+                <div class="card compact bordered w-auto">
+                    <figure>
+                        <img src="{IMG_URL}{media.poster_path}" alt="haha du kan ikke se"/>
+                    </figure>
+                    <div class="-space-x-4 avatar-group">
+                        {#each media.specific_providers as provider}
+                            <div class="avatar">
+                                <div class="w-12 h-12">
+                                    <img src="{IMG_URL}{provider.logo_path}">
+                                </div>
+                            </div>
+                        {/each}
+                    </div>
+                </div>
+            {/each}
+        </div>
+    {/if}
 </div>
 
-<Footer />
+
+<Footer/>
