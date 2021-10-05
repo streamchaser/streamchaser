7da21e6f0947dae0bd2f59a25f9ef120d260162c
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2021-10-05 19:17:41 +0200

    vite settings for websocket

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,19 @@
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+        kit: {
+                // hydrate the <div id="svelte"> element in src/app.html
+                target: '#svelte',
+                vite: {
+                        server: {
+                                hmr: {
+                                        host: 'localhost',  // host url
+                                        port: 80,           // 443 for https
+                                        protocol: 'ws',     // wss for https
+                                },
+                        },
+                },
+        },
+}
 
 export default config;
