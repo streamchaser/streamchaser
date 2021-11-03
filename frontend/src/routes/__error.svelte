01cf084a13562ce33ec14ca6b276b798871f94c1
Frederik Vincent Primdahl <frederikvincentprimdahl@pop-os.localdomain>
2021-11-03 18:44:42 +0100

    Fix the error page loading in firefox on succesful requests by delaying

--- frontend/src/routes/__error.svelte
+++ frontend/src/routes/__error.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
-    import Navbar from '../components/navbar.svelte';
-    import Footer from '../components/footer.svelte';
-    import Error from '../components/error.svelte';
+    import Navbar from "../components/navbar.svelte";
+    import Footer from "../components/footer.svelte";
+    import Error from "../components/error.svelte";
+    import { onMount } from "svelte";
 
+    // This is a temp fix for the firefox dyanmic import bug
+    // TODO: Find a proper way to fix the dynamic import issue a remove this
+    let loadError = false;
+
+    onMount(async () => {
+        setTimeout(() => (loadError = true), 100);
+    });
 </script>
 
 <svelte:head>
-    <title>404 Page not found</title>
+    {#if loadError}
+        <title>404 Page not found</title>
+    {/if}
 </svelte:head>
 
-<div class="flex flex-col h-screen justify-between">
-    <Navbar />
-    <Error error={'404 Page not found'} />
-    <Footer/>
-</div>
+{#if loadError}
+    <div class="flex flex-col h-screen justify-between">
+        <Navbar />
+        <Error error={"404 Page not found"} />
+        <Footer />
+    </div>
+{/if}
