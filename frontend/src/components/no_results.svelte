ecd0250b2734d6154478ed4c01b16e1713bbb522
Frederik <frederik.vincent@hotmail.com>
2021-11-04 12:19:49 +0100

    Add help for user by showing [consider x]

--- frontend/src/components/no_results.svelte
+++ frontend/src/components/no_results.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex flex-col">
-    <div class="m-auto text-center max-w-xl">
+    <div class="m-auto text-center max-w-md">
         <p>No results for:</p>
         <p><b><i>{input ? input : "<No input>"}</i></b></p>
         {#if currentGenres.length > 0}
@@ -13,12 +13,14 @@
             {#each currentGenres as genre}
                 <div class="badge mx-1">{genre}</div>
             {/each}
+                <p class="text-xs pt-1"><i>Consider using less genres</i></p>
         {/if}
         {#if currentProviders.length > 0}
             <p class="pt-2">Providers:</p>
             {#each currentProviders as provider}
                 <div class="badge mx-1">{provider}</div>
             {/each}
+                <p class="text-xs pt-1"><i>Consider adding more providers or removing all</i></p>
         {/if}
     </div>
 </div>
