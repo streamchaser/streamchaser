fb97339f48ba4b387ac9f7f63403d64f793b8765
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-01-21 02:40:47 +0100

    Add chunk to MeiliSearch index

--- backend/app/db/database_service.py
+++ backend/app/db/database_service.py
@@ -4,6 +4,8 @@ from app.config import get_settings
 from app.db import crud
 from app.db import database
 from app.db import models
+from app.db.database import engine
+from app.db.models import Media
 from app.db.search import client
 from sqlalchemy.exc import IntegrityError
 from tqdm import tqdm
@@ -75,54 +77,59 @@ def format_genres() -> None:
         crud.update_genre_name(db, genre)
 
 
-def init_meilisearch_indexing():
+def init_meilisearch_indexing(chunk_size: int):
     """MeiliSearch indexing from Postgres DB
     """
-    supported_country_codes = get_settings().supported_country_codes
 
-    for country_code in supported_country_codes:
-        db = database.SessionLocal()
-        all_media_iter = crud.get_all_media_iter(db=db)
-        db.close()
+    for country_code in get_settings().supported_country_codes:
+        with engine.connect() as conn:
+            media_stream = conn.execution_options(stream_results=True).execute(
+                Media.__table__.select()
+            )
 
-        client.index(f'media_{country_code}').add_documents([
-            schemas.Media(
-                id=media.id,
-                title=media.title,
-                original_title=media.original_title,
-                overview=media.overview,
-                release_date=media.release_date,
-                genres=media.genres,
-                poster_path=media.poster_path,
-                popularity=media.popularity,
-                specific_provider_names=[
-                    provider.get(country_code).get('provider_name')
-                    for provider in media.providers
-                    if provider.get(country_code)
-                ],
-                specific_providers=[
-                    provider.get(country_code)
-                    for provider in media.providers
-                    if provider.get(country_code)
-                ]
-            ).dict()
-            for media in all_media_iter
-        ])
+            print(f"Queuing up {country_code} media for MeiliSearch indexing")
 
-        extract_unique_providers_to_txt(all_media_iter, country_code)
+            while chunk := media_stream.fetchmany(chunk_size):
+                client.index(f'media_{country_code}').add_documents([
+                    schemas.Media(
+                        id=media.id,
+                        title=media.title,
+                        original_title=media.original_title,
+                        overview=media.overview,
+                        release_date=media.release_date,
+                        genres=media.genres,
+                        poster_path=media.poster_path,
+                        popularity=media.popularity,
+                        specific_provider_names=[
+                            provider.get(country_code).get('provider_name')
+                            for provider in media.providers
+                            if provider.get(country_code)
+                        ],
+                        specific_providers=[
+                            provider.get(country_code)
+                            for provider in media.providers
+                            if provider.get(country_code)
+                        ]
+                    ).dict()
+                    for media in chunk
+                ])
 
 
-def extract_unique_providers_to_txt(media_list, country_code):
-    provider_set = {
-        provider.get(country_code).get('provider_name')
-        for media in media_list
-        for provider in media.providers
-        if provider.get(country_code)
-    }
-    ordered_provider_list = sorted(provider_set)
-    with open(f'../providers_{country_code}.txt', 'w') as file:
-        for provider in ordered_provider_list:
-            file.write(f'{provider}\n')
+def extract_unique_providers_to_txt():
+    db = database.SessionLocal()
+    media_list = crud.get_all_media(db=db)
+
+    for country_code in get_settings().supported_country_codes:
+        provider_set = {
+            provider.get(country_code).get('provider_name')
+            for media in media_list
+            for provider in media.providers
+            if provider.get(country_code)
+        }
+        ordered_provider_list = sorted(provider_set)
+        with open(f'../providers_{country_code}.txt', 'w') as file:
+            for provider in ordered_provider_list:
+                file.write(f'{provider}\n')
 
 
 def prune_non_ascii_media_from_db():
