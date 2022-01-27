33efea1970b51ccd431085513eb76e8795721a9a
Frederik Primdahl <50198099+Primdahl26@users.noreply.github.com>
2022-01-27 16:24:59 +0100

    Go to top button (#132)
    * Add go to top button
    
    * Make functions es6 syntax
    
    * Add opacity

---
+++ frontend/src/components/back_to_top_button.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let showOnPx = 150
+  let hidden = true
+
+  const goTop = () => {
+    document.body.scrollIntoView({ behavior: "smooth" })
+  }
+
+  const scrollContainer = () => {
+    return document.documentElement || document.body
+  }
+
+  const handleOnScroll = () => {
+    if (!scrollContainer()) {
+      return
+    }
+
+    if (scrollContainer().scrollTop > showOnPx) {
+      hidden = false
+    } else {
+      hidden = true
+    }
+  }
+</script>
+
+<svelte:window on:scroll={handleOnScroll} />
+
+<div class="fixed top-0 left-0 p-5 transition-all opacity-80">
+  <button class="btn btn-secondary shadow-md" on:click={goTop} class:hidden>▲</button>
+</div>
