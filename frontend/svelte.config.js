1ec1c3c25c1e9340169754d27981efbd5afc9cb8
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-01-27 20:05:01 +0100

    Setup Node adapter + move app_env to root .env

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,11 +3,13 @@ import svelte from "svelte-preprocess"
 import autoPreprocess from "svelte-preprocess"
 import sveltePreprocess from "svelte-preprocess"
 import typescript from "@rollup/plugin-typescript"
+import adapter from "@sveltejs/adapter-node"
 
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    adapter: adapter(),
     vite: {
       server: {
         hmr: {
