41a44b373b0f8c98df60f4072301c2d5d7951d73
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-01-27 21:41:48 +0100

    Move repeating frontend code into __layout.svelte

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,5 +1,17 @@
 <script>
   import "../app.css"
+  import Navbar from "../components/navbar.svelte"
+  import Footer from "../components/footer.svelte"
+  import BackToTopButton from "../components/back_to_top_button.svelte"
+  import CookieDisclaimer from "../components/cookie_disclaimer.svelte"
 </script>
 
-<slot />
+<div class="flex flex-col h-screen justify-between">
+  <Navbar />
+  <div class="container mb-auto mx-auto">
+    <slot />
+  </div>
+  <BackToTopButton />
+  <Footer />
+</div>
+<CookieDisclaimer />
