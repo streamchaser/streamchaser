d84b38f6c568d18c2d1310508eb83a2e0263c805
AndreasPB <andreas.p.brodersen@gmail.com>
2022-02-17 13:22:30 +0100

    Fix weird versioning errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,8 +34,6 @@ async def init_db():
 
 streamchaser_url = get_settings().streamchaser_url
 
-print(get_settings().tmdb_key)
-
 
 if get_settings().app_environment == Environment.PRODUCTION:
     origins = [
