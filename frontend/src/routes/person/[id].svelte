68f04f8e9263e61ff14f075a8e8ba296a1153e83
Frederik <frederik.vincent@hotmail.com>
2022-02-25 17:09:10 +0100

    Make most popular backdrop_path win on person details no matter if movie or tv

--- frontend/src/routes/person/[id].svelte
+++ frontend/src/routes/person/[id].svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { sortListByPopularity } from "../../utils"
   import { variables } from "../../variables.js"
   import { page } from "$app/stores"
   import Error from "../../components/error.svelte"
   import TopCard from "../../components/details/top_card.svelte"
   import PersonMedia from "../../components/details/person_media.svelte"
   import Spinner from "../../components/loading/spinner.svelte"
+  import TopCard from "../../components/details/top_card.svelte"
 
   const PERSON_DETAIL_URL: string = `${variables.apiPath}/person/${$page.params.id}`
 
@@ -14,6 +16,11 @@
     for (let i = 0; i < mediaArray.length; i++) {
       mediaArray[i].id = mediaType.charAt(0) + mediaArray[i].id
     }
+  }
+
+  const getMostPopularBackdropPath = mediaList => {
+    sortListByPopularity(mediaList)
+    return mediaList[0].backdrop_path
   }
 
   const fetchPersonDetails = async () => {
@@ -43,7 +50,9 @@
   <Spinner />
 {:then person}
   <TopCard
-    backdropPath={person.movie_credits[0].backdrop_path}
+    backdropPath={getMostPopularBackdropPath(
+      person.movie_credits.concat(person.tv_credits)
+    )}
     posterPath={person.profile_path}
     title={person.name}
     overview={person.biography}
