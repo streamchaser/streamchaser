55f134a386ff4a148988ef2f3a7fb5bbc51a59d5
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-03-05 15:54:35 +0100

    Fix delayed page load from transition

--- frontend/src/components/no_results.svelte
+++ frontend/src/components/no_results.svelte
@@ -6,36 +6,27 @@
   export let currentProviders: string[]
   export let currentGenres: Genre[]
   export let input: string
-
-  let visible = false
-
-  onMount(() => {
-    // Lets the fade transition happen before it renders
-    setTimeout(() => (visible = true), 200)
-  })
 </script>
 
-{#if visible}
-  <div in:fade={{ duration: 200 }} class="flex flex-col mt-20">
-    <div class="m-auto text-center max-w-md">
-      <p>No results for:</p>
-      <p><b><i>{input ? input : "<No input>"}</i></b></p>
-      {#if currentGenres.length > 0}
-        <p class="pt-2">Genres:</p>
-        {#each currentGenres as genre}
-          <div class="badge mx-1">{genre.label}</div>
-        {/each}
-        <p class="text-xs pt-1"><i>Consider using less genres</i></p>
-      {/if}
-      {#if currentProviders.length > 0}
-        <p class="pt-2">Providers:</p>
-        {#each currentProviders as provider}
-          <div class="badge mx-1">{provider.label}</div>
-        {/each}
-        <p class="text-xs pt-1">
-          <i>Consider adding more providers or removing all</i>
-        </p>
-      {/if}
-    </div>
+<div in:fade={{ duration: 200 }} class="flex flex-col mt-20">
+  <div class="m-auto text-center max-w-md">
+    <p>No results for:</p>
+    <p><b><i>{input ? input : "<No input>"}</i></b></p>
+    {#if currentGenres.length > 0}
+      <p class="pt-2">Genres:</p>
+      {#each currentGenres as genre}
+        <div class="badge mx-1">{genre.label}</div>
+      {/each}
+      <p class="text-xs pt-1"><i>Consider using less genres</i></p>
+    {/if}
+    {#if currentProviders.length > 0}
+      <p class="pt-2">Providers:</p>
+      {#each currentProviders as provider}
+        <div class="badge mx-1">{provider.label}</div>
+      {/each}
+      <p class="text-xs pt-1">
+        <i>Consider adding more providers or removing all</i>
+      </p>
+    {/if}
   </div>
-{/if}
+</div>
