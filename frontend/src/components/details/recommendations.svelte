0ce1b223d6279630a372500996f055f1f1f1b2db
Mikael <mikaelpasovski@mail.com>
2022-03-17 13:54:30 +0100

    Added check on amount of slides to show according to screensize
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Replaced daisyui carousel with swiper component

--- frontend/src/components/details/recommendations.svelte
+++ frontend/src/components/details/recommendations.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
-  import { mediaIdToUrlConverter } from "../../utils"
+  import { calculateAmountOfShownItems, mediaIdToUrlConverter } from "../../utils"
   import type { Recommendation } from "../types"
+  import { Swiper, SwiperSlide } from "swiper/svelte"
+  import "swiper/css"
+
+  let slidesPerView = calculateAmountOfShownItems({
+    width: window.visualViewport.width,
+    xxl: 7,
+    xl: 6,
+    lg: 5,
+    md: 4,
+    sm: 3,
+    mobile: 2,
+  })
 
   const IMG_URL = "https://image.tmdb.org/t/p/original/"
 
@@ -9,26 +21,26 @@
 </script>
 
 {#if recommendations.length}
-  <h1 class="text-center text-3xl pt-5">Recommendations</h1>
-  <div class="pt-5">
-    <div
-      class="p-4 space-x-4 carousel carousel-center bg-neutral-focus sm:shadow-md sm:rounded-box"
-    >
+  <h1 class="text-center text-3xl pt-5 pb-5">Recommendations</h1>
+  <div class="bg-neutral rounded-lg">
+    <Swiper spaceBetween={15} {slidesPerView}>
       {#each recommendations as recommendation}
         {#if recommendation.poster_path}
-          <a
-            href={mediaIdToUrlConverter(recommendation.id, mediaType)}
-            target="_self"
-            class="carousel-item h-96 w-64 p-1"
-          >
-            <img
-              src="{IMG_URL}{recommendation.poster_path}"
-              class="rounded-lg hover:contrast-75 hover:ring-2 ring-primary"
-              alt={recommendation.title}
-            />
-          </a>
+          <SwiperSlide>
+            <a
+              href={mediaIdToUrlConverter(recommendation.id, mediaType)}
+              target="_self"
+              class="p-1"
+            >
+              <img
+                src="{IMG_URL}{recommendation.poster_path}"
+                class="h-80 rounded-lg hover:contrast-75 hover:ring-2 ring-primary"
+                alt={recommendation.title}
+              />
+            </a>
+          </SwiperSlide>
         {/if}
       {/each}
-    </div>
+    </Swiper>
   </div>
 {/if}
