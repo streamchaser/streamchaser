3bd2a4543de8db3d2426a6f7485cf01c1be8a013
Olligagman <mikaelpasovski@gmail.com>
2022-03-20 14:06:01 +0100

    Replacing mediaquery view logic with single slider view for seasons

--- frontend/src/components/details/seasons.svelte
+++ frontend/src/components/details/seasons.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import ReadMore from "./read_more.svelte"
-  import MediaQuery from "svelte-media-query"
+  import { calculateAmountOfShownItems } from "../../utils"
+  import { Swiper, SwiperSlide } from "swiper/svelte"
+  import "swiper/css"
 
   export let seasons
 
@@ -10,6 +12,15 @@
 
   let currentTab: number = 0
   let currentOverviewLength: number = INITIAL_OVERVIEW_LENGTH
+  let slidesPerView = calculateAmountOfShownItems({
+    width: window.visualViewport.width,
+    xxl: 7,
+    xl: 6,
+    lg: 5,
+    md: 4,
+    sm: 3,
+    mobile: 2,
+  })
 
   const changeActiveTab = index => {
     currentTab = index
@@ -38,88 +49,37 @@
 
   <div class="my-4 flex justify-center">
     {#each seasons as season, index}
-      <MediaQuery query="(max-width: 1024px)" let:matches>
-        {#if matches}
-          {#if index === currentTab}
-            <div class="card shadow-lg image-full w-auto sm:w-4/6 md:w-3/5 ">
-              {#if season.poster_path}
-                <figure>
-                  <img
-                    src="{LOW_RES_IMG_URL}{season.poster_path}"
-                    class="object-fit rounded-lg"
-                    alt={season.name}
-                  />
-                </figure>
-              {:else}
-                <figure>
-                  <img
-                    src="../static/no_image_available.jpg"
-                    class="object-fit rounded-lg"
-                    alt="No poster available"
-                  />
-                </figure>
-              {/if}
-              <div class="card-body">
-                <div class="card-title">{season.name}</div>
-                <div class="text-xl text-netural-content">
-                  {season.air_date ? season.air_date.split("-")[0] : "No air date"}
-                  | {season.episode_count} episodes
-                </div>
-                <div class="text-lg text-netural-content">
-                  {season.air_date ? `Premiered on ${season.air_date}` : "Hasn't aired"}
-                </div>
-                &nbsp
-                <div class="text-netural-content">
-                  {season.overview ? season.overview : "No season overview available."}
-                </div>
-              </div>
-            </div>
-          {/if}
-        {/if}
-      </MediaQuery>
-      <MediaQuery query="(min-width: 1025px)" let:matches>
-        {#if matches}
-          {#if index === currentTab}
-            <div class="card card-side bg-neutral-focus bordered lg:w-5/6 xl:w-3/5">
-              {#if season.poster_path}
-                <figure>
-                  <img
-                    src="{LOW_RES_IMG_URL}{season.poster_path}"
-                    class="object-fit rounded-lg h-96"
-                    alt={season.name}
-                  />
-                </figure>
-              {:else}
-                <figure>
-                  <img
-                    src="../static/no_image_available.jpg"
-                    class="object-fit rounded-lg h-86"
-                    alt="No poster available"
-                  />
-                </figure>
-              {/if}
-              <div class="mx-4 my-2">
-                <div class="text-xl text-netural-content">
-                  {season.air_date ? season.air_date.split("-")[0] : "No air date"}
-                  | {season.episode_count} episodes
-                </div>
-                &nbsp
-                <div class="text-lg text-netural-content">
-                  {season.air_date ? `Premiered on ${season.air_date}` : "Hasn't aired"}
-                </div>
-                &nbsp
-                <ReadMore
-                  currentDescriptionLength={currentOverviewLength}
-                  mediaDescription={season.overview
-                    ? season.overview
-                    : "No season overview available."}
-                  initialDescriptionLength={INITIAL_OVERVIEW_LENGTH}
-                />
-              </div>
-            </div>
-          {/if}
-        {/if}
-      </MediaQuery>
+      {#if season.poster_path}
+        <figure>
+          <img
+            src="{LOW_RES_IMG_URL}{season.poster_path}"
+            class="object-fit rounded-lg"
+            alt={season.name}
+          />
+        </figure>
+      {:else}
+        <figure>
+          <img
+            src="../static/no_image_available.jpg"
+            class="object-fit rounded-lg"
+            alt="No poster available"
+          />
+        </figure>
+      {/if}
+      <div class="card-body">
+        <div class="card-title">{season.name}</div>
+        <div class="text-xl text-netural-content">
+          {season.air_date ? season.air_date.split("-")[0] : "No air date"}
+          | {season.episode_count} episodes
+        </div>
+        <div class="text-lg text-netural-content">
+          {season.air_date ? `Premiered on ${season.air_date}` : "Hasn't aired"}
+        </div>
+        &nbsp
+        <div class="text-netural-content">
+          {season.overview ? season.overview : "No season overview available."}
+        </div>
+      </div>
     {/each}
   </div>
 </div>
