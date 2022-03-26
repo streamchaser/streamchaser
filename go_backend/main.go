7fbc6c6df4954908a9d41f3407e4735581ee1691
nullxDEADBEEF <andxdgu@gmail.com>
2022-03-26 04:30:33 +0100

    Add origins based on app environment

--- go_backend/main.go
+++ go_backend/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,9 +17,31 @@ var Rdb = redis.NewClient(&redis.Options{
 	DB:   0,
 })
 
+const (
+	PRODUCTION  = "prod"
+	DEVELOPMENT = "dev"
+)
+
 func main() {
+	environment := os.Getenv("APP_ENVIRONMENT")
+	streamchaserUrl := os.Getenv("HOST_NAME")
+
+	var origins []string
+
+	if environment == PRODUCTION {
+		origins = []string{fmt.Sprintf("http://%s", streamchaserUrl),
+			fmt.Sprintf("https://%s", streamchaserUrl)}
+	} else {
+		origins = []string{"*"}
+	}
+
 	app := gin.Default()
-	app.Use(cors.Default())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     origins,
+		AllowCredentials: true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+	}))
 	go app.GET("/genres/", GetGenres)
 	app.Run("0.0.0.0:9001")
 }
