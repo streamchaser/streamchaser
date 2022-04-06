c97f534d96f4e0e953d275728ff7f2e22a4664bf
AndreasPB <andreas.p.brodersen@gmail.com>
2022-04-06 15:10:20 +0200

    Add GORM and some basic DB ideas

--- go_internal/go.mod
+++ go_internal/go.mod
@@ -1,3 +1,20 @@
 module blazingly-fast-providers
 
 go 1.18
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.11.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.10.0 // indirect
+	github.com/jackc/pgx/v4 v4.15.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gorm.io/driver/postgres v1.3.3 // indirect
+	gorm.io/gorm v1.23.4 // indirect
+)
--- go_internal/main.go
+++ go_internal/main.go
@@ -5,28 +5,50 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Media struct {
+	Id    int    `json:"id"`
 	Title string `json:"title"`
 }
 
+type DBMedia struct {
+	gorm.Model
+	Id    int
+	Title string
+}
+
 func main() {
+	dsn := "host=localhost user=postgres password=postgres dbname=streamchaser port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to DB!")
+	}
+
+	// For testing
+	// db.Exec("DELETE FROM db_media")
+
+	db.AutoMigrate(&DBMedia{})
+	db.Create(&DBMedia{Id: 123, Title: "Not LOTR"})
+
 	var wg sync.WaitGroup
 
 	// TODO: going to receive ids from python
 	ids := []string{"120", "122"}
 	for _, id := range ids {
 		wg.Add(1)
-		go fetchMovies(&wg, id)
+		go fetchMovies(&wg, db, id)
 	}
 
 	wg.Wait()
 }
 
-func fetchMovies(wg *sync.WaitGroup, id string) Media {
+func fetchMovies(wg *sync.WaitGroup, db *gorm.DB, id string) Media {
 	defer wg.Done()
-	res, err := http.Get(fmt.Sprintf("https://api.themoviedb.org/3/movie/%s?api_key=THE_KEY&language=en-US", id))
+	res, err := http.Get(fmt.Sprintf("https://api.themoviedb.org/3/movie/%s?api_key=<TMDB_KEY>&language=en-US", id))
 	if err != nil {
 		panic(err)
 	}
@@ -40,7 +62,15 @@ func fetchMovies(wg *sync.WaitGroup, id string) Media {
 		panic(err)
 	}
 
+	db.Create(&DBMedia{Id: media.Id, Title: media.Title})
+
 	fmt.Println(media)
+
+	// For testing
+	var dbMedia DBMedia
+	db.First(&dbMedia, 120)
+	a, err := json.Marshal(dbMedia)
+	fmt.Println(string(a))
 
 	return media
 }
