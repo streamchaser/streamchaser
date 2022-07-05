dc7831f795652ad6fe6e080652eab9e741e01617
Frederik <fv@magenta-aps.dk>
2022-07-05 17:54:34 +0200

    Credit JustWatch in footer, and make TMDb logo come from url instead of static

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -17,19 +17,29 @@
     </div>
     <div>
       <p class="pb-5">
-        Copyright © 2021 - All rights reserved by
+        Copyright © 2022 - All rights reserved by
         <a class="link link-hover" href="https://github.com/orgs/streamchaser"
           >Streamchaser</a
         >
       </p>
-      <p class="pb-2">
+      <p class="pb-5">
         <a href="https://www.themoviedb.org/">
           <img
             class="w-20 inline"
-            src="../../tmdb.svg/"
+            src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
             alt="The Movie Database logo"
           />
-        </a> &nbsp This product uses the TMDB API but is not endorsed or certified by TMDB
+        </a> &nbsp Streamchaser uses the TMDB API but is not endorsed or certified by TMDB
+      </p>
+      <p class="pb-2">
+        <a href="https://www.justwatch.com/">
+          <img
+            class="w-20 inline"
+            src="https://www.justwatch.com/appassets/img/logo/JustWatch-logo-large.webp"
+            alt="JustWatch logo"
+          />
+        </a> &nbsp Streamchaser gets provider data from JustWatch but is not endored or certified
+        by JustWatch
       </p>
     </div>
   </div>
