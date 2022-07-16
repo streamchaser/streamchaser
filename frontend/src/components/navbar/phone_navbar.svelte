7133620bdbc08c7026607c204ddb19bd969c8c83
Frederik Primdahl <50198099+Primdahl26@users.noreply.github.com>
2022-07-16 18:48:45 +0200

    Buttons to close on click (#265)

--- frontend/src/components/navbar/phone_navbar.svelte
+++ frontend/src/components/navbar/phone_navbar.svelte
@@ -2,6 +2,18 @@
 <script lang="ts">
   import CountrySelector from "../country_selector.svelte"
   import ThemeSelector from "../theme_selector.svelte"
+
+  let isDropdownOpen = false
+
+  const handleDropdownClick = () => {
+    isDropdownOpen = !isDropdownOpen
+  }
+
+  const handleDropdownFocusLost = ({ relatedTarget, currentTarget }) => {
+    if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget))
+      return
+    isDropdownOpen = false
+  }
 </script>
 
 <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content max-h-16">
@@ -17,25 +29,40 @@
       <a class="btn btn-ghost btn-sm rounded-btn" href="/faq"> FAQ </a>
     </div>
     <ThemeSelector />
-    <div class="dropdown dropdown-end">
-      <label tabindex="0" class="btn btn-ghost btn-sm rounded-btn text-xl">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h7"
-          /></svg
-        >
+    <div class="dropdown dropdown-end" on:focusout={handleDropdownFocusLost}>
+      <label
+        tabindex="0"
+        class="btn btn-ghost btn-sm rounded-btn text-xl"
+        on:click={handleDropdownClick}
+      >
+        {#if isDropdownOpen}
+          <svg
+            class="swap-on fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 512 512"
+            ><polygon
+              points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
+            /></svg
+          >
+        {:else}
+          <svg
+            class="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 512 512"
+            ><path
+              d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"
+            /></svg
+          >
+        {/if}
       </label>
       <ul
         tabindex="0"
         class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        style:visibility={isDropdownOpen ? "visible" : "hidden"}
       >
         <li>
           <CountrySelector />
@@ -44,11 +71,16 @@
         <li><a href="/about">About</a></li>
         <li>
           <a href="https://github.com/streamchaser/streamchaser">
-            <img
-              class="w-6"
-              src="../../GitHub-Mark-Light-120px-plus.png"
-              alt="GitHub link"
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              ><path
+                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+              /></svg
+            >
           </a>
         </li>
       </ul>
--- frontend/src/components/theme_selector.svelte
+++ frontend/src/components/theme_selector.svelte
@@ -4,16 +4,63 @@
   import { chosenTheme } from "../stores/theme.js"
   import { THEMES } from "../variables.js"
 
+  let isDropdownOpen = false
+
+  const handleDropdownClick = () => {
+    isDropdownOpen = !isDropdownOpen
+  }
+
+  const handleDropdownFocusLost = ({ relatedTarget, currentTarget }) => {
+    if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget))
+      return
+    isDropdownOpen = false
+  }
+
   onMount(() => {
     themeChange(false)
   })
 </script>
 
-<div class="dropdown dropdown-end">
-  <div tabindex="0" class="btn btn-ghost btn-sm rounded-btn text-xl">🎨</div>
+<div class="dropdown dropdown-end" on:focusout={handleDropdownFocusLost}>
+  <div
+    tabindex="0"
+    class="btn btn-ghost btn-sm rounded-btn text-xl"
+    on:click={handleDropdownClick}
+  >
+    {#if isDropdownOpen}
+      <svg
+        class="swap-on fill-current"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 512 512"
+        ><polygon
+          points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
+        /></svg
+      >
+    {:else}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        ><rect x="3" y="3" width="18" height="18" rx="2" /><circle
+          cx="8.5"
+          cy="8.5"
+          r="1.5"
+        /><path d="M20.4 14.5L16 10 4 20" /></svg
+      >
+    {/if}
+  </div>
   <ul
     tabindex="0"
     class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+    style:visibility={isDropdownOpen ? "visible" : "hidden"}
   >
     {#each THEMES as theme}
       <li
