a11b17c92649475a250adaa6721acd72edd4fab1
Frederik <fv@magenta-aps.dk>
2022-07-17 16:59:40 +0200

    Add filter amount indicator

--- frontend/src/components/filters.svelte
+++ frontend/src/components/filters.svelte
@@ -3,6 +3,7 @@
 
   export let search: () => void
   let isDropdownOpen = false
+  $: filterAmount = Object.values($filters).filter(item => item === false).length
 
   const handleDropdownClick = () => {
     isDropdownOpen = !isDropdownOpen
@@ -15,107 +16,112 @@
   }
 </script>
 
-<div class="dropdown dropdown-end" on:focusout={handleDropdownFocusLost}>
-  <label tabindex="0" class="btn btn-primary ml-2" on:click={handleDropdownClick}>
-    {#if isDropdownOpen}
-      <svg
-        class="swap-on fill-current"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 512 512"
-        ><polygon
-          points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
-        /></svg
-      >
-    {:else}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        ><line x1="4" y1="21" x2="4" y2="14" /><line
-          x1="4"
-          y1="10"
-          x2="4"
-          y2="3"
-        /><line x1="12" y1="21" x2="12" y2="12" /><line
-          x1="12"
-          y1="8"
-          x2="12"
-          y2="3"
-        /><line x1="20" y1="21" x2="20" y2="16" /><line
-          x1="20"
-          y1="12"
-          x2="20"
-          y2="3"
-        /><line x1="1" y1="14" x2="7" y2="14" /><line
-          x1="9"
-          y1="8"
-          x2="15"
-          y2="8"
-        /><line x1="17" y1="16" x2="23" y2="16" /></svg
-      >
-    {/if}
-  </label>
-  <div
-    tabindex="0"
-    class="dropdown-content card card-compact w-64 p-2 shadow bg-base-100 text-primary-content mt-1"
-    style:visibility={isDropdownOpen ? "visible" : "hidden"}
-  >
-    <div class="card-body">
-      <h3 class="card-title text-neutral-content">Filters</h3>
-      <div class="divider" />
-      <h3 class="text-neutral-content"><b>Content types</b></h3>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text">Movies</span>
-          <input
-            type="checkbox"
-            class="toggle"
-            bind:checked={$filters.movieChecked}
-            on:change={() => {
-              if (!$filters.movieChecked) {
-                $filters.tvChecked = true
-              }
-              search()
-            }}
-          />
-        </label>
-        <label class="label cursor-pointer">
-          <span class="label-text">TV Shows</span>
-          <input
-            type="checkbox"
-            class="toggle"
-            bind:checked={$filters.tvChecked}
-            on:change={() => {
-              if (!$filters.tvChecked) {
-                $filters.movieChecked = true
-              }
-              search()
-            }}
-          />
-        </label>
-      </div>
-      <div class="divider" />
-      <h3 class="text-neutral-content"><b>No providers</b></h3>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text"
-            >{$filters.showNoProviders ? "Showing" : "Hiding"}</span
-          >
-          <input
-            type="checkbox"
-            class="toggle"
-            bind:checked={$filters.showNoProviders}
-            on:change={() => search()}
-          />
-        </label>
+<div class="indicator">
+  {#if filterAmount}
+    <span class="indicator-item badge badge-secondary">{filterAmount}</span>
+  {/if}
+  <div class="dropdown dropdown-end" on:focusout={handleDropdownFocusLost}>
+    <label tabindex="0" class="btn btn-primary ml-2" on:click={handleDropdownClick}>
+      {#if isDropdownOpen}
+        <svg
+          class="swap-on fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 512 512"
+          ><polygon
+            points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
+          /></svg
+        >
+      {:else}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          ><line x1="4" y1="21" x2="4" y2="14" /><line
+            x1="4"
+            y1="10"
+            x2="4"
+            y2="3"
+          /><line x1="12" y1="21" x2="12" y2="12" /><line
+            x1="12"
+            y1="8"
+            x2="12"
+            y2="3"
+          /><line x1="20" y1="21" x2="20" y2="16" /><line
+            x1="20"
+            y1="12"
+            x2="20"
+            y2="3"
+          /><line x1="1" y1="14" x2="7" y2="14" /><line
+            x1="9"
+            y1="8"
+            x2="15"
+            y2="8"
+          /><line x1="17" y1="16" x2="23" y2="16" /></svg
+        >
+      {/if}
+    </label>
+    <div
+      tabindex="0"
+      class="dropdown-content card card-compact w-64 p-2 shadow bg-base-100 text-primary-content mt-1"
+      style:visibility={isDropdownOpen ? "visible" : "hidden"}
+    >
+      <div class="card-body">
+        <h3 class="card-title text-neutral-content">Filters</h3>
+        <div class="divider" />
+        <h3 class="text-neutral-content"><b>Content types</b></h3>
+        <div class="form-control">
+          <label class="label cursor-pointer">
+            <span class="label-text">Movies</span>
+            <input
+              type="checkbox"
+              class="toggle"
+              bind:checked={$filters.movieChecked}
+              on:change={() => {
+                if (!$filters.movieChecked) {
+                  $filters.tvChecked = true
+                }
+                search()
+              }}
+            />
+          </label>
+          <label class="label cursor-pointer">
+            <span class="label-text">TV Shows</span>
+            <input
+              type="checkbox"
+              class="toggle"
+              bind:checked={$filters.tvChecked}
+              on:change={() => {
+                if (!$filters.tvChecked) {
+                  $filters.movieChecked = true
+                }
+                search()
+              }}
+            />
+          </label>
+        </div>
+        <div class="divider" />
+        <h3 class="text-neutral-content"><b>No providers</b></h3>
+        <div class="form-control">
+          <label class="label cursor-pointer">
+            <span class="label-text"
+              >{$filters.showNoProviders ? "Showing" : "Hiding"}</span
+            >
+            <input
+              type="checkbox"
+              class="toggle"
+              bind:checked={$filters.showNoProviders}
+              on:change={() => search()}
+            />
+          </label>
+        </div>
       </div>
     </div>
   </div>
