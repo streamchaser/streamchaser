afe2bbeb71bb0e621648cce75ef24baa15c2f11f
AndreasPB <andreas.p.brodersen@gmail.com>
2022-07-20 22:50:42 +0200

    Pad it up!

--- frontend/src/components/country_locator.svelte
+++ frontend/src/components/country_locator.svelte
@@ -50,7 +50,7 @@
           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         /></svg
       >
-      <span>
+      <span class="px-2">
         {#if errorMsg}
           {errorMsg}
         {/if}
