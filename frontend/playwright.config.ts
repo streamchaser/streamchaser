6e6bef83dcb005feb156aa3bf2051e43dc676576
AndreasPB <andreas.p.brodersen@gmail.com>
2022-09-23 23:37:13 +0200

    fix: use the correct port for the vite preview during tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -3,8 +3,7 @@ import type { PlaywrightTestConfig } from "@playwright/test"
 const config: PlaywrightTestConfig = {
   webServer: {
     command: "yarn build && yarn preview",
-    port: 3000,
-    timeout: 90000,
+    port: 4173,
   },
   expect: {
     // timeout: 1000 * 30,  // Longer timeout for testing
