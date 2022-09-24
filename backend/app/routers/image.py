2b42378d4d7453a7180fe049d1663e942770f9e0
AndreasPB <andreas.p.brodersen@gmail.com>
2022-09-24 20:37:41 +0200

    feat: add redis caching to image proxy

--- backend/app/routers/image.py
+++ backend/app/routers/image.py
@@ -1,22 +1,26 @@
 import httpx
+from app.db.cache import redis
 from fastapi import APIRouter
-from starlette.background import BackgroundTask
-from starlette.responses import StreamingResponse
+from starlette.responses import Response
 
 
 router = APIRouter(
     prefix="/image",
-    tags=["image"],
+    tags=["image, poster"],
 )
 
 client = httpx.AsyncClient(base_url="https://image.tmdb.org/t/p/")
 
 
-# TODO: Should cache result
 @router.get("/")
-async def image_proxy(size: str = "w342", path: str = "/example.jpg"):
-    req = client.build_request("GET", f"{size}/{path}")
-    r = await client.send(req, stream=True)
-    return StreamingResponse(
-        r.aiter_raw(), background=BackgroundTask(r.aclose), headers=r.headers
-    )
+async def cached_image_proxy(size: str = "w342", path: str = "/example.jpg"):
+    if cached_image := (await redis.get(f"image:{size}_{path}")):
+        return Response(content=cached_image, media_type="image/jpg")
+
+    res = await client.get(f"{size}/{path}")
+    image: bytes = await res.aread()
+
+    await redis.set(f"image:{size}_{path}", image)
+    await redis.expire(f"image:{size}_{path}", 60 * 60 * 24)  # 1 day
+
+    return Response(content=image, media_type="image/jpg")
