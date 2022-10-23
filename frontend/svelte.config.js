8c67cdea27de6724c84208959708b01ce034a77b
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2022-10-23 19:23:59 +0200

    feat: setup tauri build + change to static adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,21 +1,20 @@
 /** @type {import('@sveltejs/kit').Config} */
 import svelte from "svelte-preprocess"
+import staticAdapter from "@sveltejs/adapter-static";
 import autoPreprocess from "svelte-preprocess"
-import sveltePreprocess from "svelte-preprocess"
+import preprocess from "svelte-preprocess"
 import typescript from "@rollup/plugin-typescript"
 import adapter from "@sveltejs/adapter-auto"
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
-  plugins: [
-    svelte({
-      preprocess: autoPreprocess(),
-    }),
-    typescript(),
-  ],
-  preprocess: sveltePreprocess(),
-}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-export default config
+  kit: {
+    adapter: staticAdapter(),
+  },
+};
+
+export default config;
