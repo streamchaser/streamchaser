c84a293bdd1f527f55b319874938d9dc007b516c
AndreasPB <andreas.p.brodersen@gmail.com>
2022-12-09 14:33:29 +0100

    feat: add meilisearch stats endpoint

---
+++ backend/app/routers/stats.py
@@ -0,0 +1,15 @@
+from app.db.search import async_client
+from fastapi import APIRouter
+from meilisearch_python_async.models.client import ClientStats
+
+
+router = APIRouter(
+    prefix="/stats",
+    tags=["stats"],
+)
+
+
+@router.get("/", response_model=ClientStats)
+async def get_all_stats() -> ClientStats:
+    """Specific Movie page"""
+    return await async_client.get_all_stats()
