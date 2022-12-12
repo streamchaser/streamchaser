a4b488b948a021157cb86fee3b55dd96564a8fc1
Frederik <fv@magenta-aps.dk>
2022-12-12 15:52:23 +0100

    feat: add sitemap that is generated from meilisearch data

--- backend/cli.py
+++ backend/cli.py
@@ -1,3 +1,7 @@
+import datetime
+import xml.etree.cElementTree as ET
+from math import floor
+
 import httpx
 import typer
 from app.api import fetch_changed_media_ids
@@ -11,6 +15,7 @@ from app.db.database_service import countries_to_redis
 from app.db.database_service import insert_genres_to_cache
 from app.db.database_service import providers_to_redis
 from app.db.database_service import remove_stale_media
+from app.db.search import async_client
 from app.db.search import client
 from app.db.search import search_client_config
 from app.util import chunkify
@@ -21,6 +26,69 @@ from tqdm import tqdm
 supported_country_codes = get_settings().supported_country_codes
 
 app = typer.Typer()
+
+
+@app.command()
+@coroutine
+async def create_sitemap(chunk_size: int = 25000):
+    total_documents = (
+        await async_client.index("media").get_stats()
+    ).number_of_documents
+    offset = 0
+    urls = {
+        "https://streamchaser.tv",
+        "https://streamchaser.tv/faq",
+        "https://streamchaser.tv/about",
+    }
+
+    typer.echo(
+        f"Creating sitemap with {total_documents} documents in chunks of {chunk_size}"
+    )
+
+    with tqdm(
+        total=floor(total_documents / chunk_size), desc="Fetching data from meilisearch"
+    ) as pbar:
+        while offset < total_documents:
+            chunked_media = await async_client.index("media").get_documents(
+                limit=chunk_size, offset=offset, fields=["id"]
+            )
+            if not chunked_media:
+                offset += chunk_size
+                continue
+
+            for media in chunked_media.results:
+                url = ""
+                if media["id"][0] == "m":
+                    url = f"https://streamchaser.tv/movie/{media['id'][1:]}"
+                else:
+                    url = f"https://streamchaser.tv/tv/{media['id'][1:]}"
+                urls.add(url)
+
+            offset += chunk_size
+            pbar.update(1)
+
+    root = ET.Element("urlset")
+    root.attrib["xmlns:xsi"] = "http://www.w3.org/2001/XMLSchema-instance"
+    root.attrib["xsi:schemaLocation"] = (
+        "http://www.sitemaps.org/schemas/sitemap/0.9 "
+        "http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    )
+    root.attrib["xmlns"] = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    for url in tqdm(urls, desc="Generating sitemap"):
+        doc = ET.SubElement(root, "url")
+        ET.SubElement(doc, "loc").text = url
+        ET.SubElement(doc, "lastmod").text = date
+        ET.SubElement(doc, "changefreq").text = "daily"
+
+    typer.echo("Writing sitemap file...")
+
+    tree = ET.ElementTree(root)
+    tree.write("./static/sitemap.xml", encoding="utf-8", xml_declaration=True)
+
+    echo_success("Successfully created sitemap")
 
 
 @app.command()
