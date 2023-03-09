8f44ecba8c45fe4af20db7dcebce84b33d9f6cf0
AndreasPB <andreas.p.brodersen@gmail.com>
2023-03-10 00:20:18 +0100

    feat: add edgedb-py dependency and create async db client

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,26 +1,10 @@
-from app.config import get_settings
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import edgedb
 
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{get_settings().psql_user}:"
-    f"{get_settings().psql_pwd}@db:5432/streamchaser"
+db_client = edgedb.create_async_client(
+    host="edgedb",
+    port=5656,
+    tls_security="insecure",
+    database="edgedb",
+    user="edgedb",
 )
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    except Exception as e:
-        print(f"Error in {__name__}: {e}")
-    finally:
-        db.close()
