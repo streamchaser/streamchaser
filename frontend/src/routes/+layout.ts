6aca2b28d97358da1da80355c41a7fec9aae62c2
Frederik <fv@magenta-aps.dk>
2023-03-18 22:22:18 +0100

    feat: add setup of profile page

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,6 +1,10 @@
 import type { main_Country } from "$lib/generated/go"
 import type { LayoutLoad } from "./$types"
 import { GO_API } from "$lib/variables"
+import { env } from "$env/dynamic/public"
+
+// TODO: Kill this with fire, when https://github.com/sveltejs/kit/issues/5606 is fixed
+export const ssr = env.PUBLIC_ENV === "prod"
 
 export const load: LayoutLoad = async ({ fetch }) => {
   const fetchCountries = async (): Promise<main_Country[]> => {
---
+++ frontend/src/routes/profile/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { auth } from "$lib/stores/stores"
+  import { parseJwt } from "$lib/utils"
+  import type { User } from "$lib/types"
+
+  let user: User
+
+  $: if ($auth) {
+    user = parseJwt($auth)
+  }
+</script>
+
+<div class="card w-96 bg-neutral shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Hi there, {user.given_name} 👋</h2>
+    <p>
+      This page is your's, and you will be able to create personal lists, choose your
+      providers, and more.
+    </p>
+  </div>
+</div>
