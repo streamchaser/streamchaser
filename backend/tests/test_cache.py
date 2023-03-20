151f31e89bb1d0e60fa609ecba8a736bc483f07e
nullxDEADBEEF <andxdgu@gmail.com>
2023-03-20 11:52:13 +0100

    refactor: rewrite test to directly test up against fix_genre_ampersand

--- backend/tests/test_cache.py
+++ backend/tests/test_cache.py
@@ -1,7 +1,14 @@
 import asyncio
 
 from app.db.cache import redis
+from app.db.database_service import fix_genre_ampersand
 from pytest import fixture
+
+test_genre_dict = {
+    1: "NoAmpersands",
+    2: "One & Ampersand",
+    3: "A & Lot & of & Ampersands",
+}
 
 
 @fixture(scope="session")
@@ -28,3 +35,13 @@ async def reset_db():
     """
     yield
     await redis.flushdb()
+
+
+async def test_fix_genre_ampersand():
+    genres = fix_genre_ampersand(test_genre_dict)
+
+    assert genres
+    assert len(genres) == 3
+    assert "&" in genres[2]["label"]
+    for genre in genres:
+        assert "&" not in genre["value"]
