8a6d8444869349f79056df293564bda381744ce9
AndreasPB <andreas.p.brodersen@gmail.com>
2023-04-15 20:10:34 +0200

    chore: change to vite preprocess and remove rollup

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,20 +1,14 @@
-/** @type {import('@sveltejs/kit').Config} */
-import svelte from "svelte-preprocess"
-import autoPreprocess from "svelte-preprocess"
-import sveltePreprocess from "svelte-preprocess"
-import typescript from "@rollup/plugin-typescript"
+import { vitePreprocess } from "@sveltejs/kit/vite"
 import nodeAdapter from "@sveltejs/adapter-node"
 import vercelAdapter from "@sveltejs/adapter-vercel"
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: process.env.VERCEL ? vercelAdapter() : nodeAdapter(),
   },
-  plugins: [
-    svelte({
-      preprocess: autoPreprocess(),
-    }),
-    typescript(),
-  ],
-  preprocess: sveltePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 }
