9ea0de59241a6e1722751b32d4a395e43a6badb2
nullxDEADBEEF <andxdgu@gmail.com>
2023-06-01 17:19:38 +0200

    refactor!: move slider implementation from swiper to splidejs

--- frontend/src/lib/components/details/recommendations.svelte
+++ frontend/src/lib/components/details/recommendations.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import type { Recommendation } from "$lib/types"
-  import { Swiper, SwiperSlide } from "swiper/svelte"
-  import { Navigation, Lazy } from "swiper"
   import MediaCard from "$lib/components/media_card.svelte"
-  import "swiper/css"
-  import "swiper/css/free-mode"
+  import { Splide, SplideSlide } from "@splidejs/svelte-splide"
+  import "@splidejs/svelte-splide/css"
   import { onMount } from "svelte"
   import { currentCountry } from "$lib/stores/preferences"
   import Spinner from "$lib/components/loading/spinner.svelte"
@@ -20,54 +18,44 @@
 </script>
 
 {#if recommendations.length && loadedPage}
-  <h1 class="text-center text-3xl pt-5 pb-5">Recommendations</h1>
+  <h1 class="pt-5 pb-5 text-3xl text-center">Recommendations</h1>
   {#await lookupMedia(recommendations, $currentCountry)}
     <Spinner timeout={false} />
   {:then lookup}
-    <div class="swiper-container px-2 mx-2">
-      <Swiper
-        style="
-        --swiper-navigation-color: text-blue-500;
-        --swiper-navigation-size: 25px;
-      "
-        grabCursor={true}
-        resistance={false}
-        preloadImages={false}
-        lazy={{
-          enabled: true,
-          checkInView: true,
-          loadPrevNext: true,
+    <div class="px-2 mx-2">
+      <Splide
+        options={{
+          type: "loop",
+          lazyLoad: "sequential",
+          pagination: false,
+          drag: "free",
+          gap: "0.1rem",
+          start: 1,
+          mediaQuery: "min",
+          breakpoints: {
+            360: { perPage: 1 },
+            640: { perPage: 2 },
+            768: { perPage: 3 },
+            1024: { perPage: 4 },
+            1280: { perPage: 5 },
+            1536: { perPage: 6 },
+          },
         }}
-        watchSlidesProgress={true}
-        breakpoints={{
-          0: { slidesPerView: 2 },
-          576: { slidesPerView: 3 },
-          640: { slidesPerView: 3.3 },
-          768: { slidesPerView: 4 },
-          1024: { slidesPerView: 5 },
-          1280: { slidesPerView: 6 },
-          1536: { slidesPerView: 7 },
-        }}
-        modules={[Navigation, Lazy]}
-        loop={true}
-        navigation={true}
-        freeMode={true}
-        touchEventsTarget={"container"}
       >
         {#each lookup.meilisearch.hits as hit, index}
           {#if hit.poster_path}
-            <SwiperSlide>
-              <div class="p-1 swiper-lazy">
+            <SplideSlide>
+              <div class="p-1">
                 <MediaCard
                   media={hit}
                   mediaIndex={index}
                   providerAmounts={lookup.providerAmounts}
                 />
               </div>
-            </SwiperSlide>
+            </SplideSlide>
           {/if}
         {/each}
-      </Swiper>
+      </Splide>
     </div>
   {/await}
 {/if}
