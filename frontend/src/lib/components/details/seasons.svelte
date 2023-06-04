112196091d2806add2925adff4c7aa50c5184f06
Andreas <andxdgu@gmail.com>
2023-06-04 23:20:38 +0200

    refactor!: move slider implementation from swiper to splidejs (#538)
    * refactor!: move slider implementation from swiper to splidejs
    
    * fix: weird spacing on low resolutions
    
    * fix: removed swiper + deps from lock file

--- frontend/src/lib/components/details/seasons.svelte
+++ frontend/src/lib/components/details/seasons.svelte
@@ -1,90 +1,82 @@
 <script lang="ts">
   import ReadMore from "./read_more.svelte"
-  import { Swiper, SwiperSlide } from "swiper/svelte"
-  import { Navigation, Lazy } from "swiper"
+  import { Splide, SplideSlide } from "@splidejs/svelte-splide"
+  import "@splidejs/svelte-splide/css"
   import { IMG_W342 } from "$lib/variables"
-  import "swiper/css"
-  import "swiper/css/free-mode"
-  import "swiper/css/navigation"
   import type { TV } from "$lib/generated"
 
   export let seasons: TV["seasons"]
 </script>
 
 <div class="container p-2">
-  <div class="text-3xl p-4 flex justify-center">Seasons</div>
-  <div class="swiper-container">
-    <Swiper
-      grabCursor={true}
-      resistance={false}
-      freeMode={true}
-      preloadImages={false}
-      lazy={{
-        enabled: true,
-        checkInView: true,
-        loadPrevNext: true,
-      }}
-      watchSlidesProgress={true}
-      style="
-              --swiper-navigation-color: text-blue-500;
-              --swiper-navigation-size: 25px;
-            "
-      spaceBetween={10}
-      breakpoints={{
-        320: { slidesPerView: 1.3 },
-        360: { slidesPerView: 1.5 },
-        480: { slidesPerView: 2 },
-        640: { slidesPerView: 2.5 },
-        768: { slidesPerView: 3 },
-        1024: { slidesPerView: 4 },
-        1280: { slidesPerView: 5 },
-        1536: { slidesPerView: 6 },
-      }}
-      navigation={true}
-      modules={[Navigation, Lazy]}
-      initialSlide={seasons[0].name === "Specials" ? 1 : 0}
-    >
-      {#each seasons as season}
-        <SwiperSlide>
-          <div
-            class="card h-[350px] aspect-[342/513] bg-base-100 image-full swiper-lazy"
-          >
-            {#if season.poster_path}
-              <figure>
-                <img
-                  data-src="{IMG_W342}{season.poster_path}"
-                  class="swiper-lazy"
-                  alt={season.name}
+  <div class="flex justify-center p-4 text-3xl">Seasons</div>
+  <Splide
+    options={{
+      lazyLoad: "sequential",
+      drag: "free",
+      snap: true,
+      focus: "center",
+      pagination: false,
+      gap: "1rem",
+      start: 1,
+      omitEnd: true,
+      mediaQuery: "min",
+      breakpoints: {
+        1536: { perPage: 6 },
+        1280: { perPage: 5 },
+        1024: { perPage: 4 },
+        768: { perPage: 3 },
+        640: { perPage: 2 },
+        480: { perPage: 1 },
+        320: { perPage: 1 },
+      },
+    }}
+  >
+    {#each seasons as season}
+      <SplideSlide class="splide-seasons">
+        <div class="card h-[350px] aspect-[342/513] bg-base-100 image-full">
+          {#if season.poster_path}
+            <figure>
+              <img
+                data-splide-lazy="{IMG_W342}{season.poster_path}"
+                alt={season.name}
+              />
+            </figure>
+          {:else}
+            <figure class="bg-base-100" />
+          {/if}
+          <div class="overflow-auto p-4 card-body">
+            <div class="card-title">{season.name}</div>
+            <div class="text-xl text-neutral-content">
+              {season.air_date ? season.air_date.split("-")[0] : "No air date"}
+              | {season.episode_count} episodes
+            </div>
+            <div class="text-lg text-neutral-content">
+              {season.air_date ? `Premiered ${season.air_date}` : "Hasn't aired"}
+            </div>
+            <div class="pt-2 text-neutral-content">
+              {#if season.overview}
+                <ReadMore
+                  currentDescriptionLength={150}
+                  mediaDescription={season.overview}
+                  initialDescriptionLength={150}
                 />
-                <div class="swiper-lazy-preloader-white" />
-              </figure>
-            {:else}
-              <figure class="bg-base-100" />
-            {/if}
-            <div class="card-body overflow-auto p-4">
-              <div class="card-title">{season.name}</div>
-              <div class="text-xl text-neutral-content">
-                {season.air_date ? season.air_date.split("-")[0] : "No air date"}
-                | {season.episode_count} episodes
-              </div>
-              <div class="text-lg text-neutral-content">
-                {season.air_date ? `Premiered ${season.air_date}` : "Hasn't aired"}
-              </div>
-              <div class="pt-2 text-neutral-content">
-                {#if season.overview}
-                  <ReadMore
-                    currentDescriptionLength={150}
-                    mediaDescription={season.overview}
-                    initialDescriptionLength={150}
-                  />
-                {:else}
-                  No season overview available.
-                {/if}
-              </div>
+              {:else}
+                No season overview available.
+              {/if}
             </div>
           </div>
-        </SwiperSlide>
-      {/each}
-    </Swiper>
-  </div>
+        </div>
+      </SplideSlide>
+    {/each}
+  </Splide>
 </div>
+
+<style global>
+  @media (min-width: 320px) {
+    :global(.splide-seasons) {
+      display: flex;
+      justify-content: center;
+    }
+  }
+</style>
